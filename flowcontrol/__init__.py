0e1d176758a732c4ab482963ef79b7686dd6c15c
Christina <mayr9@hm.edu>
2021-02-25 09:55:01 +0100

    remove git artifact

--- flowcontrol/__init__.py
+++ flowcontrol/__init__.py
@@ -4,10 +4,8 @@ import flowcontrol.crownetcontrol.traci
 import flowcontrol.strategy
 
 # __version__ is attribute by convention
-<<<<<<< HEAD
-__version__ = 0.1
-=======
-__version__ = "0.0.1"#flowcontrol.version.Version.v_short
 
->>>>>>> stsc
+__version__ = "0.0.1"
+
+
 
