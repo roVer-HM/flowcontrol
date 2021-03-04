6f15ed789af6ca1c1d52b499ed0a0951d2e37fd0
Christina <mayr9@hm.edu>
2021-03-04 17:01:34 +0100

    rm tiktokcontroller

--- flowcontrol/crownetcontrol/controller/dummy_controller.py
+++ flowcontrol/crownetcontrol/controller/dummy_controller.py
@@ -30,32 +30,3 @@ class Controller:
         pass
 
 
-class TikTokController(Controller):
-
-    def __init__(self):
-        super().__init__()
-        self.sub = VaderePersonListener.with_vars("persons",
-                                                  {"pos": tc.VAR_POSITION, "target_list": tc.VAR_TARGET_LIST},
-                                                  init_sub=True)
-        self.control = [(1.0, ["2"]), (5.0, ["3"]), (10, ["2"]), (15, ["3"]), (20, ["2"]), (25, ["3"]), (30, ["2"])]
-        self.count = 0
-
-    def initialize_connection(self, con_manager):
-        self.con_manager = con_manager
-        self.con_manager.register_subscription_listener("vadere1", self.sub, set_default=True)
-
-    def handle_sim_step(self, sim_time, sim_state, traci_client):
-        if self.count >= len(self.control):
-            return
-        print(f"TikTokController: {sim_time} handle_sim_step evaluate control...")
-
-        print(f"TikTokController: {sim_time} apply control action ")
-        for ped_id in self.sub.pedestrian_id_list:
-            self.con_manager.domains.v_person.set_target_list(str(ped_id), self.control[self.count][1])
-
-        self.con_manager.next_call_at(self.control[self.count][0])
-        self.count += 1
-
-    def handle_init(self, sim_time, sim_state, traci_client):
-        print("TikTokController: handle_init")
-        pprint.pprint(sim_state)
