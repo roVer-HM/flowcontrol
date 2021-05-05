297246f1213cbb40540064fa5af1ef9590b2cb4f
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2021-05-05 21:05:22 +0200

    add set_default to registered state listener

--- flowcontrol/crownetcontrol/controller/dummy_controller.py
+++ flowcontrol/crownetcontrol/controller/dummy_controller.py
@@ -17,8 +17,8 @@ class Controller:
             raise RuntimeError("Controller has not working connection")
         self.con_manager.start(*kw, **kwargs)
 
-    def register_state_listener(self, name, listener):
-        self.con_manager.register_state_listener(name, listener)
+    def register_state_listener(self, name, listener, set_default=False):
+        self.con_manager.register_state_listener(name, listener, set_default)
 
     @abc.abstractmethod
     def handle_sim_step(self, sim_time, sim_state):
